c7f6e1e4d71d325df333495bdb140b339cfbe7c4
Andy Nguyen <163912633+Andyggggg@users.noreply.github.com>
2024-04-20 11:27:49 -0600

    Added Maks edition to the code

--- book.py
+++ book.py
@@ -1,3 +1,4 @@
+#Quan 1.0v 
 class Book:
     genres = {
         0: "Romance",
@@ -61,3 +62,68 @@ class Book:
 #insert set_below
     def __str__(self):
         return f"{self.__isbn} {self.__title:>20} {self.__author:>25} {self.get_genre_name():>30} {self.get_availability():>15}"
+    
+#Maksym Gordiienko 1.0v
+import csv
+ 
+class Book:
+    
+    def __init__(self, isbn, title, author, genre, available):
+        self.__isbn = isbn
+        self.__title = title
+        self.__author = author
+        self.__genre = genre
+        self.__available = available
+ 
+    genre_names = [
+        "Romance", "Mystery", "Science Fiction", "Thriller", "Young Adult",
+        "Children’s Fiction", "Self-help", "Fantasy", "Historical Fiction", "Poetry"]
+ 
+    #getters
+    def get_isbn(self):
+        return self.__isbn
+ 
+    def get_title(self):
+        return self.__title
+ 
+    def get_author(self):
+        return self.__author
+ 
+    def get_genre(self):
+        return self.__genre
+ 
+    def get_availability(self):
+        return "Available" if self.__available else "Borrowed"
+ 
+    def get_genre_name(self):
+        if 0 <= self.__genre < len(Book.genre_names):
+            return Book.genre_names[self.__genre]
+        else:
+            return "Unknown Genre"
+        
+    #setters    
+    def set_isbn(self, isbn):
+        self.__isbn = isbn
+ 
+    def set_title(self, title):
+        self.__title = title
+ 
+    def set_author(self, author):
+        self.__author = author
+ 
+    def set_genre(self, genre):
+        self.__genre = genre
+ 
+    
+    def borrow_it(self):
+        self.__available = False
+ 
+    def return_it(self):
+        self.__available = True
+ 
+    def __str__(self):
+        return f"{self.__isbn} {self.__title} {self.__author} {self.get_availability()}"
+ 
+if __name__ == "__main__":
+ 
+#import csv file
